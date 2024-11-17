8cf4effc8a63269516813642b7793b691b5c235f
lyubeznova <polina.lyubeznova@geogracom.com>
2024-11-17 16:36:44 +0300

    feature: install swagger doc

--- server/src/users/dto/create-user.dto.ts
+++ server/src/users/dto/create-user.dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ example: 'test@gmail.com', description: 'Почта' })
   readonly email: string;
+  @ApiProperty({ example: '1234', description: 'Пароль' })
   readonly password: string;
 }
